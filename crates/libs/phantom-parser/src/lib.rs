df88663cc3168deb4a07bdb31c40410db69b7cce
Alisson porto <dev.alysson@gmail.com>
2025-01-18 14:24:52 -0300

    feat: implement namespace and class body, statement to property

--- crates/libs/phantom-parser/src/lib.rs
+++ crates/libs/phantom-parser/src/lib.rs
@@ -2,13 +2,12 @@ use chumsky::{
     error::Rich,
     extra::Err,
     input::{Input, Stream, ValueInput},
-    prelude::{end, just},
+    prelude::{choice, just, recursive},
     recursive, select,
     span::SimpleSpan,
     IterParser, Parser,
 };
 use logos::Logos;
-use serde_json::json;
 
 pub type Span = SimpleSpan;
 pub type Spanned<T> = (T, Span);
@@ -21,10 +20,10 @@ pub enum Token<'a> {
     #[regex(r"\n+", |lex| lex.slice().len())]
     Newline(usize),
 
-    #[regex(r"//[^\n]*")]
+    #[regex(r"//[^\n]*", logos::skip)]
     SingleLineComment,
 
-    #[regex(r"/\*[^*]*\*+(?:[^/*][^*]*\*+)*/")]
+    #[regex(r"/\*[^*]*\*+(?:[^/*][^*]*\*+)*/", logos::skip)]
     MiltLineComment,
 
     #[token("\\")]
@@ -45,8 +44,17 @@ pub enum Token<'a> {
     #[token("class")]
     Class,
 
-    #[regex(r"(\$[a-zA-Z_][a-zA-Z0-9_]*)")]
-    Variable,
+    #[token("abstract")]
+    Abstract,
+
+    #[token("public")]
+    #[token("private")]
+    #[token("protected")]
+    Visibility(&'a str),
+
+    // Replace $
+    #[regex(r"(\$[a-zA-Z_][a-zA-Z0-9_]*)", |lex| &lex.slice()[1..])]
+    Variable(&'a str),
 
     #[regex(r"[0-9]+")]
     Number(&'a str),
@@ -80,11 +88,23 @@ pub enum Token<'a> {
 pub enum Statement<'a> {
     Namespace {
         path: Vec<&'a str>,
+        with_brackets: bool,
+        body: Vec<Statement<'a>>,
     },
+
+    Property {
+        name: &'a str,
+        value: &'a str,
+        visibility: &'a str,
+    },
+
     Class {
         is_final: bool,
-        name: String,
-        properties: Vec<&'a str>,
+        is_abstract: bool,
+        extends: Option<&'a str>,
+        implements: Vec<&'a str>,
+        name: &'a str,
+        body: Vec<Statement<'a>>,
     },
 }
 
@@ -113,23 +133,85 @@ fn statement<'a, I>() -> impl Parser<'a, I, Statement<'a>, Err<Rich<'a, Token<'a
 where
     I: ValueInput<'a, Token = Token<'a>, Span = Span>,
 {
-    let new_line = select! { Token::Newline(n) => n }
-        .map_with(|count, e| (count, e.span()))
-        .validate(|(count, span), _, emmiter| {
-            if count > 2 {
-                emmiter.emit(Rich::custom(span, "Too many new lines"));
-            }
-        });
+    recursive(|statement| {
+        let new_line = select! { Token::Newline(n) => n }
+            .map_with(|count, e| (count, e.span()))
+            .validate(|(count, span), _, emitter| {
+                if count > 2 {
+                    emitter.emit(Rich::custom(span, "Too many new lines"));
+                }
+            });
 
-    let base_path = select! { Token::Identifier(name) => name };
-    let path = base_path.separated_by(just(Token::Backslash)).collect().labelled("Path");
+        let base_path = select! { Token::Identifier(name) => name };
+        let path = base_path.separated_by(just(Token::Backslash)).collect().labelled("Path");
 
-    let namespace = just(Token::Namespace)
-        .ignore_then(path)
-        .then_ignore(just(Token::Semicolon))
-        .map(|path| Statement::Namespace { path });
+        let namespace_without_brackets = just(Token::Namespace)
+            .ignore_then(path.clone())
+            .then_ignore(just(Token::Semicolon))
+            .then(
+                statement.clone().repeated().collect::<Vec<_>>().try_map(|body, span| {
+                    if body.iter().any(|stmt| matches!(stmt, Statement::Namespace { .. })) {
+                        Err(Rich::custom(
+                            span,
+                            "Nested namespaces are not allowed in this context.",
+                        ))
+                    } else {
+                        Ok(body)
+                    }
+                }),
+            )
+            .map(|(path, body)| Statement::Namespace {
+                path,
+                with_brackets: false,
+                body,
+            });
 
-    namespace.padded_by(new_line)
+        let namespace_with_brackets = just(Token::Namespace)
+            .ignore_then(path.clone())
+            .then(
+                statement
+                    .clone()
+                    .repeated()
+                    .collect::<Vec<_>>()
+                    .delimited_by(just(Token::LBrace), just(Token::RBrace)),
+            )
+            .map(|(path, body)| Statement::Namespace {
+                path,
+                with_brackets: true,
+                body,
+            });
+
+        let namespace = namespace_with_brackets.or(namespace_without_brackets);
+
+        let property = select! {Token::Visibility(visibility) => visibility}
+            .then(select! { Token::Variable(name) => name })
+            .then_ignore(just(Token::Semicolon))
+            .map(|(visibility, name)| Statement::Property {
+                name,
+                value: "",
+                visibility,
+            });
+
+        let class = just(Token::Class)
+            .ignore_then(select! { Token::Identifier(name) => name })
+            .then(
+                statement
+                    .clone()
+                    .repeated()
+                    .collect::<Vec<_>>()
+                    .delimited_by(just(Token::LBrace), just(Token::RBrace)),
+            )
+            .map(|(name, body)| Statement::Class {
+                is_final: false,
+                is_abstract: false,
+                extends: None,
+                implements: vec![],
+                name,
+                body,
+            });
+
+        choice((class, namespace, property)).padded_by(new_line.repeated())
+    })
 }
 
 pub fn parse(source: &str) {
