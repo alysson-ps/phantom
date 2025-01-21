403f96e874f1870ddb1c4ad281bd18b7c987aef4
Alisson porto <dev.alysson@gmail.com>
2025-01-21 01:25:11 -0300

    feat: partial parser function

--- crates/libs/phantom-parser/src/lib.rs
+++ crates/libs/phantom-parser/src/lib.rs
@@ -3,7 +3,7 @@ use chumsky::{
     extra::Err,
     input::{Input, Stream, ValueInput},
     prelude::{choice, just, recursive},
-    recursive, select,
+    select,
     span::SimpleSpan,
     IterParser, Parser,
 };
@@ -47,14 +47,22 @@ pub enum Token<'a> {
     #[token("abstract")]
     Abstract,
 
+    #[token("static")]
+    Static,
+
+    #[token("function")]
+    Function,
+
     #[token("public")]
     #[token("private")]
     #[token("protected")]
     Visibility(&'a str),
 
-    // Replace $
     #[regex(r"(\$[a-zA-Z_][a-zA-Z0-9_]*)", |lex| &lex.slice()[1..])]
     Variable(&'a str),
+
+    #[token("null")]
+    Null,
 
     #[regex(r"[0-9]+")]
     Number(&'a str),
@@ -78,6 +86,24 @@ pub enum Token<'a> {
     #[token("}")]
     RBrace,
 
+    #[token("(")]
+    LParen,
+
+    #[token(")")]
+    RParen,
+
+    #[token("[")]
+    LBracket,
+
+    #[token("]")]
+    RBracket,
+
+    #[token(",")]
+    Comma,
+
+    #[token("return")]
+    Return,
+
     #[token("=")]
     Equals,
 
@@ -98,11 +124,22 @@ pub enum Statement<'a> {
         visibility: &'a str,
     },
 
+    Method {
+        name: &'a str,
+        is_static: bool,
+        visibility: &'a str,
+        args: Vec<&'a str>,
+        body: Vec<Statement<'a>>,
+    },
+
+    // Return {
+    //     expr: Expr<'a>,
+    // },
+
     Class {
         is_final: bool,
         is_abstract: bool,
         extends: Option<&'a str>,
-        implements: Vec<&'a str>,
         name: &'a str,
         body: Vec<Statement<'a>>,
     },
@@ -129,11 +166,26 @@ where
         })
 }
 
-fn statement<'a, I>() -> impl Parser<'a, I, Statement<'a>, Err<Rich<'a, Token<'a>>>>
+// fn expr_parser<'a, I>(
+// ) -> impl Parser<'a, I, Spanned<Expr<'a>>, Err<Rich<'a, Token<'a>, Span>>> + Clone
+// where
+//     I: ValueInput<'a, Token = Token<'a>, Span = Span>,
+// {
+// }
+
+fn statement<'a, I>() -> impl Parser<'a, I, Statement<'a>, Err<Rich<'a, Token<'a>, Span>>> + Clone
 where
     I: ValueInput<'a, Token = Token<'a>, Span = Span>,
 {
     recursive(|statement| {
+        // let expr_inline = recursive(|expr|{
+        //     let value = select! {
+        //         Token::Null => Expr::Value(Value::Null)
+        //     }.labelled("Value");
+
+        //     value.as_context()
+        // });
+
         let new_line = select! { Token::Newline(n) => n }
             .map_with(|count, e| (count, e.span()))
             .validate(|(count, span), _, emitter| {
@@ -205,12 +257,67 @@ where
                 is_final: false,
                 is_abstract: false,
                 extends: None,
-                implements: vec![],
                 name,
                 body,
             });
 
-        choice((class, namespace, property)).padded_by(new_line.repeated())
+        let args = select! {Token::Variable(name) => name}
+            .separated_by(just(Token::Comma))
+            .allow_trailing()
+            .collect::<Vec<_>>()
+            .delimited_by(just(Token::LParen), just(Token::RParen))
+            .labelled("Args");
+
+        let function = select! { Token::Visibility(visibility) => visibility }
+            .labelled("Visibility")
+            .then(just(Token::Static).or_not())
+            .then_ignore(just(Token::Function))
+            .then(select! { Token::Identifier(name) => name })
+            .then(args)
+            .then(
+                statement
+                    .clone()
+                    .repeated()
+                    .collect()
+                    .delimited_by(just(Token::LBrace), just(Token::RBrace)).or_not(),
+            )
+            .map_with(
+                |((((visibility, is_static), name), args), body), _| Statement::Method {
+                    name,
+                    is_static: is_static.is_some(),
+                    visibility,
+                    args,
+                    body: body.unwrap_or_default(),
+                },
+            );
+
+        // let expr = recursive(|expr| {
+        //     // Valores básicos
+        //     let value = select! {
+        //         Token::Null => Expr::Value(Value::Null),
+        //         Token::Bool(value) => Expr::Value(Value::Bool(value)),
+        //         Token::Number(value) => Expr::Value(Value::Num(value)),
+        //         Token::String(value) => Expr::Value(Value::Str(value)),
+        //     };
+
+        //     // Atribuição
+        //     let assign = select! { Token::Variable(name) => name }
+        //         .then_ignore(just(Token::Equals))
+        //         .then(expr.clone()) // Usando `expr` recursivamente
+        //         .then_ignore(just(Token::Semicolon))
+        //         .map(|(var, val)| Expr::Assign(var, Box::new(val))); // Retorno formatado
+
+        //     // Combinação de parsers
+        //     value.or(assign).map_with(|ex, e| (ex, e.span()))
+        // });
+
+        // // Parser para `return`
+        // let _return = just(Token::Return)
+        //     .ignore_then(expr.clone()) // O que está sendo retornado
+        //     .then_ignore(just(Token::Semicolon)) // Ignorando o ponto e vírgula
+        //     .map(|(ret_expr, span)| Statement::Return { expr: ret_expr }); // Retorno formatado
+
+        choice((class, function, namespace, property)).padded_by(new_line.repeated())
     })
 }
 
