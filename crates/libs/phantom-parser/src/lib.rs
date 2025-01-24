9bbe51bb664c2c02fc1c776683a889e29fd7911b
Alisson porto <dev.alysson@gmail.com>
2025-01-24 01:29:33 -0300

    feat: initial return and struct expr

--- crates/libs/phantom-parser/src/lib.rs
+++ crates/libs/phantom-parser/src/lib.rs
@@ -2,7 +2,7 @@ use chumsky::{
     error::Rich,
     extra::Err,
     input::{Input, Stream, ValueInput},
-    prelude::{choice, just, recursive},
+    prelude::{choice, just, nested_delimiters, recursive, via_parser},
     select,
     span::SimpleSpan,
     IterParser, Parser,
@@ -105,7 +105,40 @@ pub enum Token<'a> {
     Return,
 
     #[token("=")]
-    Equals,
+    Assign,
+
+    #[token("==")]
+    Eq,
+
+    #[token("!=")]
+    NotEq,
+
+    #[token("&&")]
+    And,
+
+    #[token("||")]
+    Or,
+
+    #[token("+")]
+    Plus,
+
+    #[token("-")]
+    Minus,
+
+    #[token("*")]
+    Asterisk,
+
+    #[token("/")]
+    Slash,
+
+    #[token(".")]
+    Concat,
+
+    #[token("->")]
+    Arrow,
+
+    #[token("=>")]
+    DoubleArrow,
 
     Error,
 }
@@ -128,14 +161,19 @@ pub enum Statement<'a> {
         name: &'a str,
         is_static: bool,
         visibility: &'a str,
-        args: Vec<&'a str>,
-        body: Vec<Statement<'a>>,
+        args: Vec<Statement<'a>>,
+        body: Vec<Expr<'a>>,
+    },
+
+    Parameter {
+        name: &'a str,
+        typed: Option<&'a str>,
+        value: Option<Expr<'a>>,
     },
 
     // Return {
     //     expr: Expr<'a>,
     // },
-
     Class {
         is_final: bool,
         is_abstract: bool,
@@ -150,6 +188,52 @@ struct Program<'a> {
     kind: &'a str,
     span: Span,
     statements: Vec<Statement<'a>>,
+}
+
+#[derive(Debug, PartialEq)]
+enum Expr<'a> {
+    Error,
+    List(Vec<Self>),
+    Value(Value<'a>),
+    Variable(&'a str),
+    Property(&'a str),
+    Number(i64),
+    Binary {
+        left: Box<Self>,
+        op: BinaryOp,
+        right: Box<Self>,
+    },
+    Assignment {
+        target: Box<Self>,
+        value: Box<Self>,
+    },
+    Return {
+        expr: Box<Option<Self>>,
+    },
+    Call(Box<Self>, Vec<Self>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum Value<'a> {
+    Null,
+    Bool(&'a str),
+    Num(i64),
+    Str(&'a str),
+    List(Vec<Self>),
+    Func(&'a str),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Eq,
+    NotEq,
+    And,
+    Or,
+    Concat,
 }
 
 fn parser<'a, I>() -> impl Parser<'a, I, Program<'a>, Err<Rich<'a, Token<'a>>>>
@@ -261,12 +345,42 @@ where
                 body,
             });
 
-        let args = select! {Token::Variable(name) => name}
+        let _arg = select! {Token::Variable(name) => name}
+            .then_ignore(just(Token::Assign).or_not())
+            .then(expression().or_not());
+
+        let args = _arg
             .separated_by(just(Token::Comma))
             .allow_trailing()
             .collect::<Vec<_>>()
             .delimited_by(just(Token::LParen), just(Token::RParen))
+            .map(|names| {
+                names
+                    .into_iter()
+                    .map(|(name, val)| Statement::Parameter {
+                        name,
+                        typed: None,
+                        value: val,
+                    })
+                    .collect::<Vec<_>>()
+            })
             .labelled("Args");
+
+        let _return = just(Token::Return)
+            .ignore_then(expression().padded_by(new_line.repeated()).or_not())
+            .then_ignore(just(Token::Semicolon))
+            .map(|ret_expr| Expr::Return {
+                expr: Box::new(ret_expr),
+            });
+
+        let function_body = choice((
+            _return.map(Expr::from),
+            expression().then_ignore(just(Token::Semicolon)),
+        ))
+        .padded_by(new_line.repeated())
+        .repeated()
+        .collect::<Vec<_>>()
+        .delimited_by(just(Token::LBrace), just(Token::RBrace));
 
         let function = select! { Token::Visibility(visibility) => visibility }
             .labelled("Visibility")
@@ -274,20 +388,14 @@ where
             .then_ignore(just(Token::Function))
             .then(select! { Token::Identifier(name) => name })
             .then(args)
-            .then(
-                statement
-                    .clone()
-                    .repeated()
-                    .collect()
-                    .delimited_by(just(Token::LBrace), just(Token::RBrace)).or_not(),
-            )
+            .then(function_body)
             .map_with(
                 |((((visibility, is_static), name), args), body), _| Statement::Method {
                     name,
                     is_static: is_static.is_some(),
                     visibility,
                     args,
-                    body: body.unwrap_or_default(),
+                    body,
                 },
             );
 
@@ -312,12 +420,157 @@ where
         // });
 
         // // Parser para `return`
-        // let _return = just(Token::Return)
-        //     .ignore_then(expr.clone()) // O que está sendo retornado
-        //     .then_ignore(just(Token::Semicolon)) // Ignorando o ponto e vírgula
-        //     .map(|(ret_expr, span)| Statement::Return { expr: ret_expr }); // Retorno formatado
 
         choice((class, function, namespace, property)).padded_by(new_line.repeated())
+    })
+}
+
+fn expression<'a, I>() -> impl Parser<'a, I, Expr<'a>, Err<Rich<'a, Token<'a>, Span>>> + Clone
+where
+    I: ValueInput<'a, Token = Token<'a>, Span = Span>,
+{
+    recursive(|expr| {
+        let inline_expr = recursive(|inline_expr| {
+            let val = select! {
+                Token::Null => Expr::Value(Value::Null),
+                Token::Bool(x) => Expr::Value(Value::Bool(x)),
+                Token::Number(n) => Expr::Value(Value::Num(n.parse().unwrap())),
+                Token::String(s) => Expr::Value(Value::Str(s)),
+            }
+            .labelled("value");
+
+            // let ident = select! { Token::Identifier(ident) => ident };
+
+            // A list of expressions
+            let items =
+                expr.clone().separated_by(just(Token::Comma)).allow_trailing().collect::<Vec<_>>();
+
+            let variable = select! {Token::Variable(name) => name}.map(|name| Expr::Variable(name));
+
+            let _var = variable.then_ignore(just(Token::Assign)).then(inline_expr.clone()).map(
+                |(var, val)| Expr::Assignment {
+                    target: Box::new(var),
+                    value: Box::new(val),
+                },
+            );
+
+            let property = select! {Token::Variable(name) => name}
+                .then_ignore(just(Token::Arrow))
+                .then(select! {Token::Identifier(name) => name})
+                .then_ignore(just(Token::Assign))
+                .then(inline_expr.clone())
+                .map(|((_, name), val)| Expr::Assignment {
+                    target: Box::new(Expr::Property(name)),
+                    value: Box::new(val),
+                });
+
+            let list = items
+                .clone()
+                .map(Expr::List)
+                .delimited_by(just(Token::LBrace), just(Token::RBrace));
+
+            // 'Atoms' are expressions that contain no ambiguity
+            let atom = val
+                // .or(ident.map(Expr::Local))
+                .or(property)
+                .or(variable)
+                .or(_var)
+                .or(list)
+                // In Nano Rust, `print` is just a keyword, just like Python 2, for simplicity
+                // .or(just(Token::Print)
+                // .ignore_then(
+                //     expr.clone().delimited_by(just(Token::Ctrl('(')), just(Token::Ctrl(')'))),
+                // )
+                // .map(|expr| Expr::Print(Box::new(expr))))
+                .map_with(|expr, _span| expr)
+                // Atoms can also just be normal expressions, but surrounded with parentheses
+                .or(expr.clone().delimited_by(just(Token::LParen), just(Token::RParen)))
+                // Attempt to recover anything that looks like a parenthesised expression but contains errors
+                .recover_with(via_parser(nested_delimiters(
+                    Token::LParen,
+                    Token::RParen,
+                    [
+                        (Token::LBrace, Token::RBrace),
+                        (Token::LBracket, Token::RBracket),
+                    ],
+                    |span| Expr::Error,
+                )))
+                // Attempt to recover anything that looks like a list but contains errors
+                .recover_with(via_parser(nested_delimiters(
+                    Token::LBrace,
+                    Token::RBrace,
+                    [
+                        (Token::LParen, Token::RParen),
+                        (Token::LBracket, Token::RBracket),
+                    ],
+                    |span| Expr::Error,
+                )))
+                .boxed();
+
+            // Function calls have very high precedence so we prioritise them
+            let call = atom.foldl_with(
+                items
+                    .delimited_by(just(Token::LParen), just(Token::RParen))
+                    .map_with(|args, e| args)
+                    .repeated(),
+                |f, args, e| Expr::Call(Box::new(f), args),
+            );
+
+            // Product ops (multiply and divide) have equal precedence
+            let op =
+                just(Token::Asterisk).to(BinaryOp::Mul).or(just(Token::Slash).to(BinaryOp::Div));
+            let product =
+                call.clone().foldl_with(op.then(call).repeated(), |a, (op, b), e| Expr::Binary {
+                    left: Box::new(a),
+                    op,
+                    right: Box::new(b),
+                });
+
+            // Sum ops (add and subtract) have equal precedence
+            let op = just(Token::Plus).to(BinaryOp::Add).or(just(Token::Minus).to(BinaryOp::Sub));
+            let sum = product.clone().foldl_with(op.then(product).repeated(), |a, (op, b), e| {
+                Expr::Binary {
+                    left: Box::new(a),
+                    op,
+                    right: Box::new(b),
+                }
+            });
+
+            // Comparison ops (equal, not-equal) have equal precedence
+            let op = just(Token::Eq).to(BinaryOp::Eq).or(just(Token::NotEq).to(BinaryOp::NotEq));
+            let compare =
+                sum.clone().foldl_with(op.then(sum).repeated(), |a, (op, b), e| Expr::Binary {
+                    left: Box::new(a),
+                    op,
+                    right: Box::new(b),
+                });
+
+            let op = just(Token::Concat).to(BinaryOp::Concat);
+            let concat =
+                compare.clone().foldl_with(op.then(compare).repeated(), |a, (op, b), e| {
+                    Expr::Binary {
+                        left: Box::new(a),
+                        op,
+                        right: Box::new(b),
+                    }
+                });
+
+            // Logical ops (and, or) have equal precedence
+            let op = just(Token::And).to(BinaryOp::And).or(just(Token::Or).to(BinaryOp::Or));
+            let logic = concat.clone().foldl_with(op.then(concat).repeated(), |a, (op, b), e| {
+                Expr::Binary {
+                    left: Box::new(a),
+                    op,
+                    right: Box::new(b),
+                }
+            });
+
+            logic.labelled("expression").as_context()
+        });
+
+        // return parser
+
+        inline_expr
     })
 }
 
